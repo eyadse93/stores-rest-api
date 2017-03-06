0563abacb3dbd22cbaad46fd7ab67288e493cf37
eyadse93 <eyadse93@gmail.com>
2017-03-07 01:29:22 +0200

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -2,6 +2,8 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
+import sqlite3
+
 class UserRegister(Resource):
 
     parser = reqparse.RequestParser()
@@ -22,7 +24,14 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(**data)
-        user.save_to_db()
+        #user = UserModel(**data)
+        #user.save_to_db()
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+        connection.commit()
+        connection.close()
 
         return {"message": "User created successfully."}, 201
